7851c445320ae15ba2ebf2d1f3f712e37d05e099
jxdang1 <dangjennifer21@gmail.com>
2023-10-22 22:12:07 -0500

    created functions and localStorage for jss file

--- assets/script.js
+++ assets/script.js
@@ -2,22 +2,42 @@
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
 $(function () {
-    // TODO: Add a listener for click events on the save button. This code should
-    // use the id in the containing time-block as a key to save the user input in
-    // local storage. HINT: What does `this` reference in the click listener
-    // function? How can DOM traversal be used to get the "hour-x" id of the
-    // time-block containing the button that was clicked? How might the id be
-    // useful when saving the description in local storage?
-    //
-    // TODO: Add code to apply the past, present, or future class to each time
-    // block by comparing the id to the current hour. HINTS: How can the id
-    // attribute of each time-block be used to conditionally add or remove the
-    // past, present, and future classes? How can Day.js be used to get the
-    // current hour in 24-hour time?
-    //
-    // TODO: Add code to get any user input that was saved in localStorage and set
-    // the values of the corresponding textarea elements. HINT: How can the id
-    // attribute of each time-block be used to do this?
-    //
-    // TODO: Add code to display the current date in the header of the page.
+
+    document.querySelectorAll(".saveBtn").forEach(elem => elem.addEventListener("click", savedText));
+
+
+    function savedText () {
+        // retrieves the id of the parent element that contains the hour in HTML
+        var id = $(this).parent().attr("id");
+        // the previous sibling of the current element (text) gets a value when there is text in the file
+        var text = $(this).prev().val();
+        //sets the hour and user input to local storage (id) and (text)
+        localStorage.setItem(id, text);
+    }
+
+    var timeBlocks = document.querySelectorAll(".time-block");
+
+    timeBlocks.forEach(compareTime);
+    timeBlocks.forEach(pullFromStorage);
+
+    function compareTime (item) {
+        var currentTime = dayjs().hour;
+        var currentTimeBlock = item.id.slice(5);
+
+        if (currentTimeBlock == currentTime) {
+            item.classList.add("present");
+        } else if (currentTimeBlock < currentTime) {
+            item.classList.add("past");
+        } else {
+            item.classList.add("future");
+        }
+    }
+   
+
+    function pullFromStorage (item) {
+    var currentItem = localStorage.getItem(item.id);
+    item.querySelector("textarea").value = currentItem;
+}
+
+document.getElementById("currentDay").innerHTML = dayjs().format('dddd, MMMM DD');
   });
